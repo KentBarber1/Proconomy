b32f6c6b0bc973dff492f1b53df7063fcfcdb03f
Wang, Yiwei <jessicaw9801@gmail.com>
2022-01-28 18:29:41 -0500

    added mentee register screen 3

--- app/src/main/java/com/example/proconomyapp/mentee_register3.kt
+++ app/src/main/java/com/example/proconomyapp/mentee_register3.kt
@@ -5,17 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class mentee_register2 : AppCompatActivity() {
+class mentee_register3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mentee_register2)
+        setContentView(R.layout.activity_mentee_register3)
     }
     fun backClick(view: View){
-        val myIntent = Intent(this, mentee_register::class.java)
+        val myIntent = Intent(this, mentee_register2::class.java)
         startActivity(myIntent)
-    }
-    fun menteeRegister_3(view: View) {
-        val reg = Intent(this, mentee_register3::class.java)
-        startActivity(reg)
     }
 }
