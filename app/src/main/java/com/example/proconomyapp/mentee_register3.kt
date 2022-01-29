0b1553a653b243334fd59e0b48683754aba593db
Wang, Yiwei <jessicaw9801@gmail.com>
2022-01-28 22:09:15 -0500

    modified mentee register screen 3

--- app/src/main/java/com/example/proconomyapp/mentee_register3.kt
+++ app/src/main/java/com/example/proconomyapp/mentee_register3.kt
@@ -4,14 +4,39 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 
-class mentee_register3 : AppCompatActivity() {
+class mentee_register3 : AppCompatActivity() , AdapterView.OnItemClickListener {
+    private var listView:ListView ? = null
+    private var arrayAdapter:ArrayAdapter<String> ? = null
+    private var itemList = arrayOf(
+        "College graduate (Bachelor's)", "College graduate (Master's)", "College student",
+        "Employee", "Self-employed", "In between jobs"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mentee_register3)
+
+        listView = findViewById(R.id.choiceList)
+        arrayAdapter = ArrayAdapter(applicationContext,
+            android.R.layout.simple_list_item_single_choice, itemList)
+        listView?.adapter = arrayAdapter
+        listView?.choiceMode = ListView.CHOICE_MODE_SINGLE
+        listView?.onItemClickListener = this
     }
+
     fun backClick(view: View){
         val myIntent = Intent(this, mentee_register2::class.java)
         startActivity(myIntent)
     }
+
+    override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        var items:String = p0?.getItemAtPosition(p2) as String
+        Toast.makeText(applicationContext, "$items", Toast.LENGTH_LONG).show()
+    }
+
 }
