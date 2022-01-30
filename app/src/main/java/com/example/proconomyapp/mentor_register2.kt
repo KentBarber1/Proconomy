4bec91ffc96f2e5b712959cb8b60869717f8e3b1
Kent Barber <kbarber9@gatech.edu>
2022-01-30 17:44:03 -0500

    Added mentor register first page

---
+++ app/src/main/java/com/example/proconomyapp/mentor_register2.kt
@@ -0,0 +1,18 @@
+package com.example.proconomyapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class mentor_register2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mentor_register)
+    }
+
+    fun backClick(view: View) {
+        val myIntent = Intent(this, mentor_register::class.java)
+        startActivity(myIntent)
+    }
+}
