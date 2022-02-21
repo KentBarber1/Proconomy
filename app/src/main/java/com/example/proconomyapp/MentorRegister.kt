3d473edd741ed8393c87e919d60957fd074fe323
slin915 <carrie177674@gmail.com>
2022-02-20 23:32:15 -0500

    connecting slides with the rest of the activity

--- app/src/main/java/com/example/proconomyapp/MentorRegister.kt
+++ app/src/main/java/com/example/proconomyapp/MentorRegister.kt
@@ -1,3 +1,4 @@
+
 package com.example.proconomyapp
 
 import android.content.Intent
@@ -5,16 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class MentorRegister2 : AppCompatActivity() {
+class MentorRegister : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mentor_register2)
+        setContentView(R.layout.activity_mentor_register)
     }
     fun backClick(view: View) {
-        val myIntent = Intent(this, MentorRegister2::class.java)
+        val myIntent = Intent(this, MainActivity::class.java)
         startActivity(myIntent)
     }
-    fun mentorRegister3(view: View) {
+    fun mentorRegister_2(view: View) {
         val reg = Intent(this, mentor_register3::class.java)
         startActivity(reg)
     }
