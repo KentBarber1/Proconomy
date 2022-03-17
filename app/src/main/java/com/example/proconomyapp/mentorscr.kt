b8f4bcd621e4af0e9a3ea431dfec994b0ac00511
RoxyHou <55114559+RoxyHou@users.noreply.github.com>
2022-03-17 15:48:52 -0400

    Revert "Revert "Mentor profile""

---
+++ app/src/main/java/com/example/proconomyapp/mentorscr.kt
@@ -0,0 +1,29 @@
+package com.example.proconomyapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class mentorscr : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mentorscr)
+    }
+    fun backClick(view: View){
+        val myIntent = Intent(this, MainActivity::class.java)
+        startActivity(myIntent)
+    }
+
+    fun mentorRegister(view: View) {
+        val reg = Intent(this, mentor_profile::class.java)
+        startActivity(reg)
+    }
+
+    fun basic(view:View) {
+        val reg = Intent(this, mentor_3signin::class.java)
+        startActivity(reg)
+    }
+
+
+}
